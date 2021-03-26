0d3ae30c507a13e7d4e0e2efc2cf7c0cd3bbf9fb
riodovn <ladinforket3@gmail.com>
2021-03-27 01:35:07 +0700

    update authentication

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\User;
 
 class ResetPasswordController extends Controller
 {
@@ -25,7 +29,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/trang-chu';
 
     /**
      * Create a new controller instance.
@@ -36,4 +40,46 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+    public function getInfo($token){
+        if($token == null){
+            abort(404);
+        }else{
+            return view('auth.passwords.reset')->with(['token'=>$token]);
+        }
+    }
+    public function postInfo(Request $request){
+        $request->validate([
+            'token' => 'required',
+            'email' => 'email|required|exists:users',
+            'password' => 'required|min:8|confirmed|string',
+        ],[
+            'token.required' => 'Thiếu thông tin token',
+            'email.email' => 'Vui lòng nhập email chính xác',
+            'email.required' => 'Cần nhập địa chỉ email',
+            'email.exists' => 'Email không tồn tại',
+            'password.required' => 'Bạn cần nhập mật khẩu mới',
+            'password.confirmed' => 'Nhập lại mật khẩu không khớp',
+            'password.min' => 'Mật khẩu tối thiểu 8 ký tự.',
+        ]);
+        // Kiểm tra dữ liệu email token cuối cùng có khớp với token không.
+            $password_reset = DB::table('password_resets')->where('email',$request->get('email'))->latest('created_at')->first();
+            if($password_reset == null){
+                return redirect()->back()->withErrors('Không tồn tại token này.');
+            }else{
+                $token = $request->get('token');
+                $email = $request->get('email');
+                $password = $request->get('password');
+
+                if($token == $password_reset->token){
+                    $user = User::where('email',$email)->get()->first();
+                    $user->password = Hash::make($password);
+                    $user->save();
+                    session()->flash('message','Cập nhật mật khẩu thành công! Mời bạn đăng nhập.');
+                    return redirect(route('login'));
+                }else{
+                    return redirect()->back()->withErrors('Token không khớp, kiểm tra lần gửi yêu cầu khôi phục cuối cùng.');
+                }
+            }
+    }
+
 }
